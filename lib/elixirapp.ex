0a3c846047ff7212da57f08337f0a17ea8beba23
vad2der <vad.deryabin@gmail.com>
2018-02-10 15:43:36 -0700

    user input from console: 'iex -S mix', 'Elixirapp.main()'

--- lib/elixirapp.ex
+++ lib/elixirapp.ex
@@ -1,18 +1,18 @@
 defmodule Elixirapp do
   @moduledoc """
-  Documentation for Elixirapp.
+    Documentation for Elixirapp.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixirapp.hello
-      :world
-
+    Hello world.
   """
   def say_hello do
     IO.puts "Hello world"
+  end
+
+  def main() do
+    name = IO.gets("What is your name? ")
+    name = String.trim(name)
+    IO.inspect(name)
   end
 end
