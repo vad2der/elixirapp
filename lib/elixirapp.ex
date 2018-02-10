9ee89dab807cb5c29d80fe0ef5828d80bdeab26b
vad2der <vad.deryabin@gmail.com>
2018-02-10 15:53:49 -0700

    pipes, functions with different number of arguments

--- lib/elixirapp.ex
+++ lib/elixirapp.ex
@@ -6,13 +6,22 @@ defmodule Elixirapp do
   @doc """
     Hello world.
   """
-  def say_hello do
-    IO.puts "Hello world"
-  end
 
   def main() do
-    name = IO.gets("What is your name? ")
-    name = String.trim(name)
-    IO.inspect(name)
+    name = IO.gets("What is your name? ") |> String.trim
+    say_hello(name)
+  end
+
+  def say_hello("") do
+    IO.puts "Name is required!"
+    main()
+  end
+
+  def say_hello(name) do
+    IO.puts "Hello #{name}!"
+  end
+
+  def say_hello(greetings, name) do
+    IO.puts "#{greetings} #{name}!"
   end
 end
